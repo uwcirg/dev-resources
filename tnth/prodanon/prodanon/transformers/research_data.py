1a66d50c771a2e54cb290fd7fc529398834f714b
Paul Bugni <pbugni@u.washington.edu>
2025-04-14 17:44:34 -0700

    Purge patient name from cached research_data table

---
+++ tnth/prodanon/prodanon/transformers/research_data.py
@@ -0,0 +1,7 @@
+def transform(cursor, custom_dir):
+    # remove patient name from research data
+    cursor.execute(
+        "UPDATE research_data SET data = jsonb_set("
+        "  data, '{encounter,patient,display}', '\"redacted\"')"
+    )
+    print("Updating {cursor.rowcount} research_data rows")
